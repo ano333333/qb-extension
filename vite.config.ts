e3dc9dc90fdb1982a776e64e8f615565442a1dfb
ano333333 <172483903+ano333333@users.noreply.github.com>
2024-12-04 15:15:21 +0900

    feat: tailwind導入

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { crx } from "@crxjs/vite-plugin";
 import manifest from "./manifest.json" assert { type: "json" };
+import tailwindcss from "tailwindcss";
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), crx({ manifest })],
+	css: {
+		postcss: {
+			plugins: [tailwindcss],
+		},
+	},
 });
