8e1c49e5553278c2859eb3a56ef23162e731c77c
ano333333 <172483903+ano333333@users.noreply.github.com>
2024-12-09 01:57:47 +0900

    feat: extension名をdev/prodにより変化させる

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { crx } from "@crxjs/vite-plugin";
-import manifest from "./manifest.json" assert { type: "json" };
+import { crx, type ManifestV3Export } from "@crxjs/vite-plugin";
 import tailwindcss from "tailwindcss";
+
+const extensionName =
+	process.env.ENV === "production"
+		? "qb-extension"
+		: `qb-extension-${process.env.ENV}`;
+const manifest: ManifestV3Export = {
+	manifest_version: 3,
+	name: extensionName,
+	version: "1.0.0",
+	action: {
+		default_popup: "popup.html",
+	},
+};
 
 // https://vite.dev/config/
 export default defineConfig({
